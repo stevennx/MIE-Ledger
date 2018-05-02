77c980394c643d66381c6b85bab461d1761152c0
Puffnut <steven.xu1589@gmail.com>
2018-05-02 15:28:30 -0400

    Added Id to JSON Response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { status: 200, messsage: "Successfully created user" }
+      render json: { status: 200, messsage: "Successfully created user",
+                    id: @user.id }
     else
       render json: { status: 500, errors: @user.errors }
     end
