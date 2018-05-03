3a6726d0a33f0d8667ec6d82feea67c607b2a8d4
Puffnut <steven.xu1589@gmail.com>
2018-05-03 11:59:44 -0400

    Fixed error in migration

--- db/migrate/20180429224315_create_transactions.rb
+++ db/migrate/20180429224315_create_transactions.rb
@@ -8,9 +8,9 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_foreign_key :transactions, :users, column: :borrower_id
+    add_foreign_key :transactions, :users, type: :uuid, column: :borrower_id
     add_index :transactions, :borrower_id
-    add_foreign_key :transactions, :users, column: :lender_id
+    add_foreign_key :transactions, :users, type: :uuid, column: :lender_id
     add_index :transactions, :lender_id
 
   end
