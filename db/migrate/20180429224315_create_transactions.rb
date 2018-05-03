6a0aa92c55c0f7e0c67d0d1d336566523efdc6d9
Puffnut <steven.xu1589@gmail.com>
2018-05-03 11:53:08 -0400

    Changed references migration for Postgresql

--- db/migrate/20180429224315_create_transactions.rb
+++ db/migrate/20180429224315_create_transactions.rb
@@ -1,11 +1,17 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
-      t.references :borrower, foreign_key: true
-      t.references :lender, foreign_key: true
+      t.integer :borrower_id
+      t.integer :lender_id
       t.integer :amount
 
       t.timestamps
     end
+
+    add_foreign_key :transactions, :users, :column :borrower_id
+    add_index :transactions, :borrower_id
+    add_foreign_key :transactions, :users, :column :lender_id
+    add_index :transactions, :lender_id
+
   end
 end
