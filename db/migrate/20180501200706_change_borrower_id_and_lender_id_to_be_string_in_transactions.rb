3b87a21b809dc2ce94564a1efb828281e9255254
Puffnut <steven.xu1589@gmail.com>
2018-05-03 12:27:10 -0400

    Removed unnecessary migrations

--- db/migrate/20180501200706_change_borrower_id_and_lender_id_to_be_string_in_transactions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeBorrowerIdAndLenderIdToBeStringInTransactions < ActiveRecord::Migration[5.1]
-  def change
-    change_column :transactions, :borrower_id, :string
-    change_column :transactions, :lender_id, :string
-  end
-end
--- db/migrate/20180501214818_change_borrower_and_lender_id_to_integers_in_transactions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeBorrowerAndLenderIdToIntegersInTransactions < ActiveRecord::Migration[5.1]
-  def change
-    change_column :transactions, :borrower_id, :integer
-    change_column :transactions, :lender_id, :integer
-  end
-end
